eddf95bc53a0d0196a38a386355ea5272220f8e7
dabai <bxf@1017499879>
2021-11-23 21:44:23 +0800

    use kafka update

--- go/client/kafka/kafka_test.go
+++ go/client/kafka/kafka_test.go
@@ -13,8 +13,7 @@ func init() {
 }
 
 func TestGetKafkaAsyncProducer(t *testing.T) {
-	consumer, _ := sarama.NewConsumer([]string{"127.0.0.1:9092"}, nil)
-	partitionList, _ := consumer.Partitions("topic")
+	partitionList, _ := GetConsumer().Partitions("topic")
 	for partition := range partitionList {
 		pc, err := GetConsumer().ConsumePartition("topic", int32(partition), sarama.OffsetNewest)
 		if err != nil {
